5b5a86be82f69cfd56997363d3d005453241b960
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2023-05-11 20:09:00 +0100

    añslkdf

--- database/index.js
+++ database/index.js
@@ -1,6 +1,12 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(`${process.env.DIALECT}://${process.env.DB_USERNAME}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+// const sequelize = new Sequelize(`${process.env.DIALECT}://${process.env.DB_USERNAME}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+//   logging: false
+// })
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DIALECT,
   logging: false
 })
 
